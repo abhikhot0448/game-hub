76c3d725397877532e4986d2cd2e5b34d543880c
abhik <abhik@ABHIMANYU>
2024-03-15 19:24:02 +0530

    add search bar for NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Input } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwith from "./ColorModeSwith";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack justifyContent="space-between" padding="10px" spacing={4}>
       <Image src={logo} width={100} boxSize="60px" borderRadius="15px" />
+      <Input placeholder="search games" borderRadius={10} />
       <ColorModeSwith />
     </HStack>
   );
